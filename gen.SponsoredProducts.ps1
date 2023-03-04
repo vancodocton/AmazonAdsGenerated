08db68e4b326ca3c1ec83a4687bdeb2aca5589df
Nguyen Ky Duong Truong <duongtruong.nguyenky@gmail.com>
2023-03-04 14:19:11 +0700

    init

---
+++ gen.SponsoredProducts.ps1
@@ -0,0 +1,5 @@
+java -jar C:/tools/openapi-generator-cli.jar generate `
+    -i https://dtrnk0o2zy01c.cloudfront.net/openapi/en-us/dest/SponsoredProducts_prod_3p.json `
+    -g csharp-netcore `
+    -o . `
+    -c config.SponsoredProducts.json
